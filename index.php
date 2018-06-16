9414fbc704b7dd0c9633c435666bd6a50e8a5bdf
Otremba <rickrevolution79@gmail.com>
2018-06-16 14:55:54 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,32 @@
+
+
+<!DOCTYPE html>
+ <html>
+   <head>
+     <!--Import Google Icon Font-->
+     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+     <!--Import materialize.css-->
+     <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+     <link rel="stylesheet" href="css/style.css">
+     <!--Let browser know website is optimized for mobile-->
+     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+   </head>
+
+   <body>
+     <!--JavaScript at end of body for optimized loading-->
+     <script type="text/javascript" src="js/materialize.min.js"></script>
+     <nav>
+      <div class="nav-wrapper blue">
+        <a href="#" class="brand-logo">LOGO</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="#">Organizações</a></li>
+        </ul>
+      </div>
+      <!-- Avaliaçôes de organizções proximas ao local selecionado. -->
+      <div class="collection">
+        <a href="#!" class="collection-item active">Alvin</a>
+      </div>
+    </nav>
+   </body>
+ </html>
