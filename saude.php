71b07c42bd57ef29c7269adabd4fff683d3da21c
Otremba <rickrevolution79@gmail.com>
2018-06-17 10:39:48 -0300

    sinconização

--- saude.php
+++ saude.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+
+$conexao = mysqli_connect('localhost', 'root', 'bcd127', 'hackinsampa');
+ ?>
+
+
 <!DOCTYPE html>
 <html lang="pt-br" dir="ltr">
   <head>
@@ -9,21 +16,32 @@
     <div class="logo">
       <img src="img/logo.png" alt="">
     </div>
+    <?php
+    $sql = "select * from tbl_unidade_saude";
+
+    $select = mysqli_query($conexao,$sql);
+
+    while($rsVP = mysqli_fetch_array($select))
+    {
+     ?>
     <div class="post">
       <div class="imagem-post">
-        <img src="img/sp.jpg" alt="">
+        <img src="<?php echo ($rsVP['img']);?>" alt="">
 
       </div>
       <div class="titulo-post">
-        UBS - Vila prudente
+        <?php echo ($rsVP['ESTABELECI']);?>
       </div>
       <div class="titulo-post1">
-        500m - subprefeitura mooca
+        <?php echo ($rsVP['ENDERECO']);?>
       </div>
       <div class="avalia">
         8.5
       </div>
     </div>
+    <?php
+      }
+     ?>
     <div class="post">
       <div class="imagem-post">
         <img src="img/sp.jpg" alt="">
